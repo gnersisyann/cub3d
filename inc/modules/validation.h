be63a05dd567f0561401ce008136e5f96ad76b90
Garnik <g.nersisyann@gmail.com>
2025-09-14 17:05:10 +0400

    Fixed validation header structure

--- inc/modules/validation.h
+++ inc/modules/validation.h
@@ -3,8 +3,6 @@
 
 # include "cub3d.h"
 
-// TODO: fix header
-
 typedef struct s_file_content
 {
 	char	**config_lines;
@@ -17,19 +15,18 @@ void		ft_validate(int argc, char **argv, t_data *data,
 void		ft_validate_map(char *map_path, t_data *data,
 				t_file_content *content);
 
-// argument
+// Argument validation
 void		ft_validate_arguments(int argc, char **argv);
-
 void		ft_check_file_extension(char *filename, char *extension, int type);
 void		ft_check_file_exists(char *filename);
 void		ft_check_argc(int argc);
 
-// parser
+// File parsing
 void		parse_cub_file(char *filename, t_file_content *content);
-
 char		**parse_file(char *filename);
 void		ft_split_file_content(char **lines, t_file_content *content);
 
+// Map validation
 void		ft_check_config(t_file_content *content, t_data *data);
 void		ft_validate_map_structure(t_file_content *content, t_data *data);
 
