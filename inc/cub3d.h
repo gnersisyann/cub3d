01ad7971debfb7f0b428df3af71b56a5d6c44019
Garnik <g.nersisyann@gmail.com>
2025-09-14 17:04:54 +0400

    Added t_data structure field for raycasting

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -1,20 +1,33 @@
 #ifndef CUB3D_H
-#define CUB3D_H
+# define CUB3D_H
 
-typedef struct s_data {
-  void *mlx;
-  void *win;
-  int map_fd;
-  char *map_path;
-  char *north_texture;
-  char *south_texture;
-  char *west_texture;
-  char *east_texture;
-  int floor_color;
-  int ceiling_color;
-  int player_x;
-  int player_y;
-  int player_direction;
-} t_data;
+typedef struct s_data
+{
+	void	*mlx;
+	void	*win;
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+	int		map_fd;
+	char	*map_path;
+	char	*north_texture;
+	char	*south_texture;
+	char	*west_texture;
+	char	*east_texture;
+	int		floor_color;
+	int		ceiling_color;
+	double	player_x;
+	double	player_y;
+	double	dir_x;
+	double	dir_y;
+	double	plane_x;
+	double	plane_y;
+	char	**map;
+	int		map_width;
+	int		map_height;
+	char	player_direction;
+}			t_data;
 
 #endif
