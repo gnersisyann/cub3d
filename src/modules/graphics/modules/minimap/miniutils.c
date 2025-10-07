e0a1705291b8f37ef518e4ce5bd3cc78c8069419
Davhak2 <davidhakobyan014@gmail.com>
2025-10-07 20:12:24 +0400

    feat(minimap): add field of view drawing functionality and refactor player marker

--- src/modules/graphics/modules/minimap/miniutils.c
+++ src/modules/graphics/modules/minimap/miniutils.c
@@ -2,6 +2,7 @@
 #include "defines.h"
 #include "graphics.h"
 #include "mlx.h"
+#include <math.h>
 
 void	init_raw_data(t_pixel *px, t_data *data)
 {
@@ -26,22 +27,54 @@ void	put_pixel_raw(t_pixel *px)
 	*(int *)dst = px->color;
 }
 
+static void	draw_fov_120(t_data *data, int cx, int cy, t_pixel *pixel)
+{
+	int		angle;
+	int		radius;
+	int		x;
+	int		y;
+	double	rad;
+	double	player_angle;
+
+	player_angle = atan2(data->dir_y, data->dir_x);
+	pixel->color = 0xF0D3D3;
+	radius = 1;
+	while (radius <= 20)
+	{
+		angle = -60;
+		while (angle <= 60)
+		{
+			rad = player_angle + (angle * M_PI / 180.0);
+			x = cx + (int)(radius * cos(rad));
+			y = cy + (int)(radius * sin(rad));
+			pixel->x = x;
+			pixel->y = y;
+			if (pixel->x >= 0 && pixel->x < WIDTH && pixel->y >= 0
+				&& pixel->y < HEIGHT)
+				put_pixel_with_struct(pixel);
+			angle += 1;
+		}
+		radius++;
+	}
+}
+
 void	draw_player_marker(t_data *data, int dst_off_x, int dst_off_y)
 {
-	int	cx;
-	int	cy;
-	int	dy;
-	int	dx;
+	int		cx;
+	int		cy;
+	int		dx;
+	int		dy;
 	t_pixel	pixel;
 
 	cx = dst_off_x + data->minimap.view_w / 2;
 	cy = dst_off_y + data->minimap.view_h / 2;
 	init_raw_data(&pixel, data);
-	pixel.color = 0x00FF0000; // RED
-	dy = -3;
+	draw_fov_120(data, cx, cy, &pixel);
+	pixel.color = 0x00FF0000;
+	dy = -2;
 	while (++dy <= 2)
 	{
-		dx = -3;
+		dx = -2;
 		while (++dx <= 2)
 		{
 			pixel.x = cx + dx;
